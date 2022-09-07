87b149c634e8ab6cf3aae61f4e36c1441daa6d94
Yaroslav <91559310+yaroslavafenkin@users.noreply.github.com>
2022-09-07 11:49:35 +0300

    Remove unused import

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 from flask import Flask
-from flask import send_file
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return '{"ping":"pong"}'
+@app.route('/cloudview-api/rest/v1/iac/getScanList')
+def scanlist():
+    return '{"content": [{"status": "FINISHED"}]}'
 
-@app.route('/post', methods = ['POST'])
-def howToPost():
-    return 'Handle agrs like https://stackoverflow.com/a/25268170'
+@app.route('/cloudview-api/rest/v1/iac/scanResult')
+def result():
+    return '''{"result": [
+        {
+            "results": {
+                "passedChecks": [{
+                    "checkResult": {"result": "SUCCESS"},
+                    "checkId": "<img/src/onerror=alert('checkId')>",
+                    "checkName": "<img/src/onerror=alert('checkName')>",
+                    "criticality": "<img/src/onerror=alert('criticality')>",
+                    "filePath": "<img/src/onerror=alert('filePath')>",
+                    "resource": "<img/src/onerror=alert('resource')>"
+                }],
+                "failedChecks": [],
+                "parsingErrors": []
+            },
+            "checkType": "checktype",
+            "summary": {
+                "failedStats": {
+                    "high": 3,
+                    "medium": 2,
+                    "low": 1
+                },
+                "passed": 4,
+                "failed": 6,
+                "skipped": 8,
+                "parsingErrors": 111
+            }
+        }
+    ]}'''
+
+@app.route('/cloudview-api/rest/v1/iac/scan', methods = ['POST'])
+def test():
+    return '{"scanUuid": "5eef275e-dbb8-4f8d-8541-9b4c18460983"}'
